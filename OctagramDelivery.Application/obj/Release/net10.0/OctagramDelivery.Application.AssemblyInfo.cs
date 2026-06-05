2b4adcbbabd38679baa8b3f6d1e1c97cd0ab7811
Kevin Ortiz <63128557+keorgtz@users.noreply.github.com>
2026-06-05 15:51:23 -0600

    Fix: Operation 1.3

--- OctagramDelivery.Application/obj/Release/net10.0/OctagramDelivery.Application.AssemblyInfo.cs
+++ OctagramDelivery.Application/obj/Release/net10.0/OctagramDelivery.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OctagramDelivery.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b485c9b9a6cebd3d452499660ff9f0627008e24")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a592d3238c9359987482b241bd57a8cd78b86c6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("OctagramDelivery.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OctagramDelivery.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
